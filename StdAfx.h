bc9d0dffd0e67d3f939ca595252b4e1d8523c21e
fireundubh <fireundubh@gmail.com>
2018-02-26 22:24:08 -0800

    Ported to IDA 7.0 and MSVC 2017

--- StdAfx.h
+++ StdAfx.h
@@ -1,10 +1,9 @@
-
 #pragma once
 #define WIN32_LEAN_AND_MEAN
 #define WINVER       0x0601 // _WIN32_WINNT_WIN7
 #define _WIN32_WINNT 0x0601
-#include <windows.h>
-#include <stdlib.h>
+#include <Windows.h>
+#include <cstdlib>
 #include <intrin.h>
 #pragma intrinsic(memset, memcpy, strcat, strcmp, strcpy, strlen)
 
@@ -14,7 +13,9 @@
 #include <ida.hpp>
 #include <auto.hpp>
 #include <loader.hpp>
+#include <funcs.hpp>
 #pragma warning(push)
+#pragma warning(disable:4244) // "conversion from 'size_t' to 'xxx', possible loss of data"
 #pragma warning(disable:4267) // "conversion from 'size_t' to 'xxx', possible loss of data"
 #include <typeinf.hpp>
 #pragma warning(pop)
